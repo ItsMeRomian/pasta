3f96d117150f839aa7329385861489963940e910
Romian Tairovski <romian.tairovski@gmail.com>
2021-07-06 15:48:39 +0200

    new favicon and cleanup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,26 @@
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'pasta',
+    title: 'Pasta! 🍝',
     htmlAttrs: {
       lang: 'en',
     },
-    meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }, { hid: 'description', name: 'description', content: '' }, { name: 'format-detection', content: 'telephone=no' }],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }, { hid: 'description', name: 'description', content: 'Paste Code or Text, and view it later :)' }, { name: 'format-detection', content: 'telephone=no' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/spaghetti.png' }],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['@/static/style.css'],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['plugins/supabase.client.js'],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-  ],
+  buildModules: ['@nuxtjs/eslint-module'],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     [
       'nuxt-highlightjs',
@@ -49,9 +37,7 @@ export default {
     duration: 2400,
   },
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 }
